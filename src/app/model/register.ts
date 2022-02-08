06975dd3c915c5230fa98c498dd8d2085446e126
Vinayak Bora <54656826+Vinayakbora@users.noreply.github.com>
2022-02-08 08:47:39 +0530

    Add files via upload

---
+++ src/app/model/register.ts
@@ -0,0 +1,17 @@
+export class Register
+{
+    id: number;
+    name:string;
+    email:string;
+    pwd:string;
+   
+
+    constructor()
+    {
+        this.id=0;
+        this.name="";
+        this.email="";
+        this.pwd="";
+       
+    }
+}
